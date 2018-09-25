28499a146738033963899d830a86c176b9a37b31
Daiane Macedo <daiane.macedo@hotmail.com>
2018-09-25 16:14:19 -0300

    DatePicker

--- config.js
+++ config.js
@@ -1,13 +1,15 @@
 exports.config = {
   
- // seleniumAddress: 'http://localhost:4444/wd/hub',
-  baseURL: 'https://material.angularjs.org/latest/demo/datepicker',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+ // baseURL: 'https://material.angularjs.org/latest/demo/datepicker',
   capabilities: {
-    'browserName': 'firefox',
+    'browserName': 'chrome',
     'chromeOptions': {
       args: ['--lang=pt-BR', 'start-maximized'],
     },
   },
-  specs: ['./tests/todospec.js']
+  suites: {
+    parte1: ['./tests/todo.spec.js']
+  }
 
 };
